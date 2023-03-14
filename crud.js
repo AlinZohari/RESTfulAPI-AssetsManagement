bbc3b57dd80544c9aec1ddc2c0f26a6af2c2923b
AlinZohari <zohari.alin@gmail.com>
2023-03-14 19:56:04 +0000

    adding a route called /testCRUD with two endpoints -one for the GET and one for POST

--- crud.js
+++ crud.js
@@ -26,3 +26,17 @@ console.log(config);
 //add data parser functionality to the API-so that the NodeJS code can read through the individual name/value pairs that are posted by the form
 const bodyParser = require('body-parser');
 crud.use(bodyParser.urlencoded({ extended: true }));
+
+
+
+//adding a route called /testCRUD with two endpoints -one for the GET and one for POST
+
+// test endpoint for GET requests (can be called from a browser URL or AJAX)
+crud.get('/testCRUD',function (req,res) {
+res.json({message:req.originalUrl+" " +"GET REQUEST"});
+});
+
+// test endpoint for POST requests - can only be called from AJAX
+crud.post('/testCRUD',function (req,res) {
+res.json({message:req.body});
+});
